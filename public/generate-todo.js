b4617dca4ec75083a435b6277b53fcb02ea5ffa4
kkstuvstad <stuvstak@oregonstate.edu>
2016-12-06 17:30:45 -0800

    Update generate-todo.js

--- public/generate-todo.js
+++ public/generate-todo.js
@@ -1,43 +1,16 @@
 // Grab the todo template from Handlebars.
 var todoTemplate = Handlebars.templates.todo;
 
-/*
- * This function should create an HTML string representing a new todo note
- * given the information that could be in the note.
- *
- * Note that where, when, who, and details can be an empty string.  If this
- * is the case, the corresponding portion of the todo note should not be
- * included in the HTML string.
- */
-
- var content;
 function generateTodoHTML(what, where, when, who, details) {
-
-  content = '<section class="todo">';
-  content += '<h2>'+what+'</h2>';
-  content += '    <div class="todo-body">';
-
-  if(where) {
-    content += '<p class="indent-wrapped"><span class="where">where: </span>'+where+'</p>';
+  var newNote = {
+    what: what,
+    body: {
+      where: where,
+      when: when,
+      who: who,
+      details: details
+    }
   }
 
-  if(when) {
-    content += '<p class="indent-wrapped"><span class="when">when: </span>'+when+'</p>';
-  }
-
-  if(who) {
-    content += '<p class="indent-wrapped"><span class="who">who: </span>'+who+'</p>';
-  }
-
-  if(details) {
-    content += '<p>'+details+'</p>';
-  }
-
-  content += '</div>';
-  content += '<div class="dismiss-button">&times;</div>';
-  content += '</section>';
-
-
-  return content;
-
+return todoTemplate(newNote);
 }
